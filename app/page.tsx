ffd0521ff5003b8a35a48adb4d0867f960c8bd53
Faizan Khan <mrfaizan023@gmail.com>
2026-02-28 16:03:36 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    
-    <div>
+    <div className="p-10">
       <h1>faizan khan</h1>
+
+      <Button>faizan</Button>
+
     </div>
   );
 }
